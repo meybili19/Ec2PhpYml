10bd0e4baa40d84e419a1cedbf04cd7b288a4599
Meybili <meybili200219@gmail.com>
2024-12-03 12:07:02 -0500

    feat: :sparkles: Add php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Ec2 Practica</title>
+</head>
+<body>
+	<p> Mi nombre es Meybili Olivares</p>
+	<h1><?php echo gethostname();?></h1>
+</body>
+</html>
